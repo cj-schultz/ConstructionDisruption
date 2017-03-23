4b47138b57508e8018fa4808d0a1d5fe0c11ca4d
TimSeaman <tms112@pitt.edu>
2017-03-23 16:30:25 -0400

    Added Player movement and yell

---
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed;
+    public float turnSpeed;
+    public int lengthOfYell;
+    public GameObject yell;
+    Vector3 yellOffset = new Vector3(0, 7, 0);
+    private Transform t = null;
+    private Rigidbody r = null;
+    int x = 0; // frame counter
+
+    void Start ()
+    {
+        t = this.GetComponent<Transform>();
+        r = this.GetComponent<Rigidbody>();
+    }
+	
+	void Update ()
+    {
+        Vector3 direction = Vector3.zero;
+        Quaternion rot = t.rotation;
+
+        // Standard movement stuff, use force to move so he doesn't phase through walls
+        if (Input.GetKey(KeyCode.W))
+        {
+            r.AddForce(t.forward * speed, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rot *= Quaternion.AngleAxis(turnSpeed * Time.deltaTime * -1f, Vector3.up);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            r.AddForce(t.forward * speed * -1f, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rot *= Quaternion.AngleAxis(turnSpeed * Time.deltaTime * 1f, Vector3.up);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (x == 0) // only yell if no other yell is active
+            { 
+                GameObject yellBlock = Instantiate(yell, t.position + yellOffset,
+                                        t.localRotation) as GameObject;
+                x++;
+            }
+        }
+        if(x != 0) // counts frames if there is an active yell
+        {
+            x++;
+            if(x == lengthOfYell) // allows for another yell if frame count is hit
+            {
+                x = 0;
+            }
+        }
+
+        t.position += direction * Time.deltaTime * speed;
+        t.localRotation = rot;
+    }
+}
---
+++ Assets/Scripts/PlayerScripts/YellPhysics.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YellPhysics : MonoBehaviour
+{
+    public GameObject yellBlock;
+    public float speed;
+    public float yellExpansion;
+    public int lengthOfYell;
+    private Transform t = null;
+    private Rigidbody r = null;
+    int x = 0; // frame counter
+
+	void Start ()
+    {
+        t = this.GetComponent<Transform>();
+        r = this.GetComponent<Rigidbody>();
+    }  
+	
+	void Update ()
+    {
+        Vector3 direction = Vector3.zero;
+        if (x < lengthOfYell) // the yell will stay active for lengthOfYell amount of frames
+        {
+            r.AddForce(t.forward * speed, ForceMode.VelocityChange);
+            yellBlock.transform.localScale += new Vector3(yellExpansion, yellExpansion, 0);
+        }
+        else
+        {
+            Destroy(yellBlock);
+        }
+        t.position += direction * Time.deltaTime * speed;
+        x++;
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        /*
+          if yell hits worker
+            use addforce to puch him back
+          otherwise
+            stop the yell
+            so you can't yell through walls
+        */
+    }
+}
